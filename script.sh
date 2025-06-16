e1c1da2a75f83f9d069f6d294b59abb0962dfa51
Simon <simon@aspirets.com>
2025-06-16 10:40:02 +0100

    fixed typo in script.sh

--- script.sh
+++ script.sh
@@ -26,7 +26,7 @@ awk '
 
 sudo sed -i "s|/etc/ssl/certs/ca-certificates.crt|/etc/letsencrypt/live/@@DOMAIN@@/fullchain.pem|g" /etc/rsyslog.d/99-tls.conf
 sudo sed -i "s|/etc/rsyslog.d/syslog-cert.pem|/etc/letsencrypt/live/@@DOMAIN@@/cert.pem|g" /etc/rsyslog.d/99-tls.conf
-sudo sed -i "s|/etc/rsyslog.d/syslog-key.pem|/etc/letsencrypt/live/@DOMAIN@@/privkey.pem|g" /etc/rsyslog.d/99-tls.conf
+sudo sed -i "s|/etc/rsyslog.d/syslog-key.pem|/etc/letsencrypt/live/@@DOMAIN@@/privkey.pem|g" /etc/rsyslog.d/99-tls.conf
 
 
 # Reload AppArmor to apply changes
