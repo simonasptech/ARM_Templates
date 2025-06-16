dfbb1f1d0716f120b08e01dc21b06e9821657abd
Simon <simon@aspirets.com>
2025-06-16 08:09:07 +0100

    Added @@DOMAIN@@ placeholder for domain name in script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script obtains an SSL certificate for a specified domain and sets up automatic renewal using Certbot.
 
-DOMAIN="2306.aspirenet.uk"
+DOMAIN="@@DOMAIN@@"
 EMAIL="Simon@aspirets.com"
 
 # Obtain certificate
