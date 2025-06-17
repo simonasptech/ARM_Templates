2a3841c1de319d959bca5ef002bf5f11774e6859
Simon <simon@aspirets.com>
2025-06-17 09:09:04 +0100

    Added remote folder configuration for rsyslog

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# This script obtains an SSL certificate for a specified domain and sets up automatic renewal using Certbot.
+# This script contains post deployment configurations for a Debian-based system.
 
+# Installs certbot for SSL certificate management
 DOMAIN="@@DOMAIN@@"
 EMAIL="Simon@aspirets.com"
 
@@ -11,6 +12,8 @@ sudo certbot certonly --standalone -d "$DOMAIN" --agree-tos --email "$EMAIL" --n
 CRON_JOB="0 3 * * * /usr/bin/certbot renew --quiet"
 (crontab -l 2>/dev/null | grep -v 'certbot renew'; echo "$CRON_JOB") | crontab -
 
+
+# Configure rsyslog to allow the use of Let's Encrypt private keys
 CONFIG_FILE="/etc/apparmor.d/usr.sbin.rsyslogd"
 TMP_FILE=$(mktemp)
 
@@ -24,10 +27,12 @@ awk '
     { print }
 ' "$CONFIG_FILE" > "$TMP_FILE" && sudo mv "$TMP_FILE" "$CONFIG_FILE"
 
+# Amend the conf file to point to the correct certificate files
 sudo sed -i "s|/etc/ssl/certs/ca-certificates.crt|/etc/letsencrypt/live/@@DOMAIN@@/fullchain.pem|g" /etc/rsyslog.d/99-tls.conf
 sudo sed -i "s|/etc/rsyslog.d/syslog-cert.pem|/etc/letsencrypt/live/@@DOMAIN@@/cert.pem|g" /etc/rsyslog.d/99-tls.conf
 sudo sed -i "s|/etc/rsyslog.d/syslog-key.pem|/etc/letsencrypt/live/@@DOMAIN@@/privkey.pem|g" /etc/rsyslog.d/99-tls.conf
 
+# Configure permissions for the syslog group to access the private key
 realfile=$(readlink -f /etc/letsencrypt/live/@@DOMAIN@@/privkey.pem)  
 sudo chown root:syslog "$realfile"  
 sudo chmod 640 "$realfile"
@@ -37,8 +42,17 @@ sudo chmod 755 "/etc/letsencrypt/live/@@DOMAIN@@"
 sudo chmod 755 /etc/letsencrypt/archive  
 sudo chmod 755 "/etc/letsencrypt/archive/@@DOMAIN@@"
 
+# Add remote syslog configuration directory to conf file
+sudo tee -a "/etc/rsyslog.d/99-tls.conf" >/dev/null << 'EOL'
+
+$template RemoteHostLogs,"/var/log/remote/%HOSTNAME%/%PROGRAMNAME%.log"
+*.* -?RemoteHostLogs
+EOL
+
+# Download and install the NinjaOne agent
 curl https://aspire.rmmservice.eu/ws/api/v2/generic-installer/NinjaOneAgent-i64.deb -L --output NinjaOneAgent-i64.deb
 sudo TOKENID="@@TOKENID@@" dpkg -i NinjaOneAgent-i64.deb
+
 
 # Reload AppArmor to apply changes
 sudo systemctl reload apparmor
