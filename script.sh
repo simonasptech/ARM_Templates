715fefbbeae604099c5345c264ecdfab7ad07ea1
Simon <simon@aspirets.com>
2025-06-20 13:08:34 +0100

    Updated certificate paths

--- script.sh
+++ script.sh
@@ -32,8 +32,8 @@ awk '
 ' "$CONFIG_FILE" > "$TMP_FILE" && sudo mv "$TMP_FILE" "$CONFIG_FILE"
 
 # Amend the conf file to point to the correct certificate files
-sudo sed -i "s|/etc/ssl/certs/ca-certificates.crt|/etc/letsencrypt/live/@@DOMAIN@@/fullchain.pem|g" /etc/rsyslog.d/99-tls.conf
-sudo sed -i "s|/etc/rsyslog.d/syslog-cert.pem|/etc/letsencrypt/live/@@DOMAIN@@/cert.pem|g" /etc/rsyslog.d/99-tls.conf
+sudo sed -i "s|/etc/ssl/certs/ca-certificates.crt|/etc/ssl/certs/ca-certificates.crt|g" /etc/rsyslog.d/99-tls.conf
+sudo sed -i "s|/etc/rsyslog.d/syslog-cert.pem|/etc/letsencrypt/live/@@DOMAIN@@/fullchain.pem|g" /etc/rsyslog.d/99-tls.conf
 sudo sed -i "s|/etc/rsyslog.d/syslog-key.pem|/etc/letsencrypt/live/@@DOMAIN@@/privkey.pem|g" /etc/rsyslog.d/99-tls.conf
 
 # Configure permissions for the syslog group to access the private key
